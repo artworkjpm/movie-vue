739cd7698f66cc8ed26aac9305afd9872ce8921b
John Moran <johnnymoran44@hotmail.com>
2020-01-15 13:37:56 +0100

    Update About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,18 +1,25 @@
 <template>
   <div>
-    <h4>About this web app</h4>
+    <h4>
+      About this web app |
+      <a
+        href="https://github.com/artworkjpm/movie-vue"
+        target="_blank"
+      >Github repo</a>
+    </h4>
     <p class="mb-0">When I read you brief I was happy to do the test because it involved movies! :D</p>
     <p
       class="mb-0"
     >I was so keen to get making it actually didn't read the bit about "slider" until after I made the home page search layout to be the standard scrolling of search results</p>
-    <p>Upon making the sliders I found it tricky to make them mobile responsive and the animationed, therefore I have given two slider varieties.</p>
+    <p>Upon making the sliders I found it tricky to make them mobile responsive and the animated, therefore I have given two slider varieties.</p>
     <span>Things to impress:</span>
     <ul>
       <li>Vue-Router - each movie loads its own url using the name and id in the url</li>
+      <li>On go back from film details, the film search is cached</li>
       <li>Mobile Responsive - using Bootstrap-vue to speed up the styling</li>
       <li>Load spinners - nice touch</li>
       <li>Type and press enter to see results</li>
-      <li>Slider2 is handcoded with no pluggins or frameworks and animated with start, stop</li>
+      <li>Slider2 is handcoded with no pluggins or frameworks and animated with start, stop buttons</li>
     </ul>
     <h4>About JPM (me)</h4>
     <ul>
